43a3e331f39145470ebd30afe1d38c3a3c4d36e4
looucie <lucie.flo.210568@gmail.com>
2025-05-20 11:08:08 +0200

    ajout des liens entre les pages

--- index.php
+++ index.php
@@ -1,11 +1,9 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-<title>Notre boutique de bonbons :]</title>
-</head>
-<body>
+
+<?php
+include_once("header.php");
+include_once("menu.php");
+?>
     
-</body>
-</html>
+<?php
+include_once("footer.php");
+?>
