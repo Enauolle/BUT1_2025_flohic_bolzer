666874c305ad0c2909a65cdee2f29f56b5fadbaa
Enauolle <louanebolzer@gmail.com>
2025-06-11 11:22:42 +0200

    Create Notesducours.php

---
+++ Notesducours.php
@@ -0,0 +1,10 @@
+<?php
+foreach($tableau as $variable_créée_pour_loccasion){
+    foreach($variable_créée_pour_loccasion as $clef => $deuxieme_valeur){
+        echo($clef);
+        echo("</br>"); 
+    }
+    
+};
+
+?>
