8ee93acf36a0176b486b00cb8ae919142e3201ac
Enauolle <louanebolzer@gmail.com>
2025-06-17 17:22:53 +0200

    oui

--- index.php
+++ index.php
@@ -4,31 +4,65 @@ include_once("menu.php");
 ?>
    
 <div class="banniere">
-    <img src="img/banniere.jpg" alt="Bannière test">
+    <img src="img/banniere.jpg" alt="Bannière">
 </div>
 
 
-<div class="titre2"> NOS BOUTIQUES</div>
+<div class="titre2"> 
+    <h2>NOS BOUTIQUES</h2>
+</div>
 <div class="boutiques">
     <div class="boutique">
         <a href="boutiques.php?id=1">
             <img src="img/img_bdd/B1" alt="La Mika-line">
-            <h2>Le Mika-line</h2>
+            <h3>Le Mika-line</h3>
         </a>
         </div>
     <div class="boutique">
         <a href="boutiques.php?id=2">
             <img src="img/img_bdd/B2" alt="Ok Bonbons">
-            <h2>Ok Bonbons</h2>
+            <h3>Ok Bonbons</h3>
         </a>
     </div>
     <div class="boutique">
         <a href="boutiques.php?id=3">
             <img src="img/img_bdd/B3" alt="Le Saccharo">
-            <h2>Le Saccharo</h2>
+            <h3>Le Saccharo</h3>
         </a>
     </div>
+</div>
 
+<div class="banniere">
+    <img src="img/banniere2.jpg" alt="Bannière">
+</div>
+
+<div class="histoire">
+    <div>
+        <img src="img/àpropos.jpg" alt="">
+    </div>
+    <div>
+        <h2>NOTRE HISTOIRE</h2>
+        <p>La confiserie est une entreprise familiale créée en 1847 par Emile Bonpère à Aix en Provence. 
+En 1985 Emile cède son entreprise à sa fille Chantal qui fera en sorte de maintenir la fabrication artisanale des sucreries à Aix malgré un contexte concurrentiel de plus en plus dur. En 2020, c'est au tour d'Annie de rejoindre l'entreprise familiale. Titulaire d'un MBA obtenu aux USA, elle décide de rentrer en France pour aider sa mère et développe une stratégie de commercialisation de la production familiale à travers un réseau international de grossistes spécialisés dans la revente des produits maisons.
+</br> Annie veille à maintenir et à valoriser le côté artisanal des productions et le savoir-faire de la maison "La Confiserie". Si aujourd'hui... de nombreux processus ont été automatisé, chaque bonbon produit par "La Confiserie" est d'une très grande qualité, toujours fabriqué avec des ingrédients sélectionnés avec soin et malgré la mécanisation, les recettes n'ont pas changé, conférant aux bonbons le goût de cette tradition chère à la famille Bonpère.
+</p>
+    </div>
+</div>
+
+
+<div class="banniere">
+    <img src="img/banniere3.jpg" alt="Bannière">
+</div>
+
+<div class="addresse">
+    <div>
+        <p>Rue Henri-Poincaré</p>
+        </br>
+        <p>54000 Nancy</p>
+    </div>
+    <div>
+        <img src="" alt="">
+    </div>
 </div>
 
 <?php
