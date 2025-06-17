9f5d29af5f61ef23406a4682a72579475e9b0e86
Enauolle <louanebolzer@gmail.com>
2025-06-17 09:51:10 +0200

    oui

--- index.php
+++ index.php
@@ -7,21 +7,23 @@ include_once("menu.php");
     <img src="img/banniere.jpg" alt="Bannière test">
 </div>
 
+
+<div class="titre2"> NOS BOUTIQUES</div>
 <div class="boutiques">
     <div class="boutique">
-        <a href="#">
+        <a href="boutiques.php?id=1">
             <img src="img/img_bdd/B1" alt="La Mika-line">
             <h2>Le Mika-line</h2>
         </a>
         </div>
     <div class="boutique">
-        <a href="#">
+        <a href="boutiques.php?id=2">
             <img src="img/img_bdd/B2" alt="Ok Bonbons">
             <h2>Ok Bonbons</h2>
         </a>
     </div>
     <div class="boutique">
-        <a href="#">
+        <a href="boutiques.php?id=3">
             <img src="img/img_bdd/B3" alt="Le Saccharo">
             <h2>Le Saccharo</h2>
         </a>
