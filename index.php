f3e34da0d875795ccf5ed6ffff62edcd818297d1
Enauolle <louanebolzer@gmail.com>
2025-06-18 11:21:36 +0200

    hover

--- index.php
+++ index.php
@@ -22,18 +22,21 @@ include_once("menu.php");
         <a href="boutiques.php?id=1">
             <img src="img/img_bdd/B1" alt="La Mika-line">
             <h3>Le Mika-line</h3>
+            <span class="voir-plus">Voir plus &gt;</span>
         </a>
         </div>
     <div class="boutique">
         <a href="boutiques.php?id=2">
             <img src="img/img_bdd/B2" alt="Ok Bonbons">
             <h3>Ok Bonbons</h3>
+            <span class="voir-plus">Voir plus &gt;</span>
         </a>
     </div>
     <div class="boutique">
         <a href="boutiques.php?id=3">
             <img src="img/img_bdd/B3" alt="Le Saccharo">
             <h3>Le Saccharo</h3>
+            <span class="voir-plus">Voir plus &gt;</span>
         </a>
     </div>
 </div>
